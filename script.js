4a835c8b542a35a650cec0bb1a35e55fcbfa5856
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-12 13:30:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let yourArray = ["one", 2, "three", true, false];
-function Array(yourArray){
-	
-}
+let yourArray = ['some string', 21, true, 'another string', 'one more string'];
+
 console.log(yourArray);
